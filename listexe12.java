a38cd6382429e0343f4d58509ea4f50f60c6b112
nathaliapa <nathalia2051@gmail.com>
2018-08-22 22:41:05 -0300

    Add files via upload

---
+++ listexe12.java
@@ -0,0 +1,30 @@
+package lista;
+
+import javax.swing.JOptionPane;
+
+public class listexe12 {
+	
+	public static void main(String arg[]){
+		
+		double re;
+		double q;
+		q=Double.parseDouble(JOptionPane.showInputDialog("digite a quantidade="));
+		double vp;
+		vp=Double.parseDouble(JOptionPane.showInputDialog("digite o valor do produto="));
+		double csd;
+		csd=vp*q;
+		if(q<100){
+			JOptionPane.showMessageDialog(null, "nenhum desconto"+csd);
+		}
+		if(q>200){
+			re = 0.10*csd;
+			JOptionPane.showMessageDialog(null,"10% de desconto"+re);
+			
+		}else{
+			re = 0.05*csd;
+			JOptionPane.showMessageDialog(null,"5% de desconto");
+		}
+		
+	}
+
+}
